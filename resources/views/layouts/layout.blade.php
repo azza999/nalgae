1d1a433714e61d802d9707bd8e13f302833d4915
azzadh999 <azzadh999@gmail.com>
2019-10-10 21:39:30 +0900

    Add QR code reader

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<title>@yield('title','날개제.kr')</title>
+	
+	
+	{{-- 메타 태그  세팅 --}}
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="title" content="날개제">
+	<meta name="keyword" content="날개제, 공군, 공군 항공과학고등학교">
+	<meta name="description" content="날개제 웹사이트">
+	
+	{{-- css --}}
+	<link rel="stylesheet" href="/public/css/app.css">	<!-- bootstrap -->
+	<link rel="stylesheet" href="/public/css/style.css">
+
+	 @yield('css')
+
+</head>
+<body>
+
+	@yield('content')
+
+	@yield('js')
+</body>
+</html>
---
+++ resources/views/reader.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+
+@section('css')
+	
+@endsection
+
+@section('content')
+	<div class="container">
+		<h1 class="text-center">QR코드 리더</h1>
+		<div class="reader-view">
+			<canvas id="canvas">
+				
+			</canvas>
+		</div>
+		<p id="loadingMessage" class="text-center">QR코드를 비춰주세요!</p>
+		<p id="outputData" class="text-center"></p>
+	</div>
+@endsection
+
+@section('js')
+	<script src="/public/js/app.js"></script>
+	<script src="/public/js/qr.js"></script>
+@endsection
+
