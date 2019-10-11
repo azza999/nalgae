234b312ac8b5003dc8b4dc0112e1878c8e6734bb
azzadh999 <azzadh999@gmail.com>
2019-10-11 22:20:43 +0900

    end

--- public/js/qr_reader.js
+++ public/js/qr_reader.js
@@ -1,110 +1,109 @@
 $(document).ready(e=>{
 
-  var video = document.getElementById("video");
+	if (location.protocol === 'http:') {
+		location.protocol = 'https:';
+	}
 
-  var canvasElement = document.getElementById("canvas");
+	var video = document.getElementById("video");
 
-  var canvas = canvasElement.getContext("2d");
+	var canvasElement = document.getElementById("canvas");
 
-  var message = document.getElementById("loadingMessage");
+	var canvas = canvasElement.getContext("2d");
 
-  var outputData = document.getElementById("outputData");
+	var message = document.getElementById("loadingMessage");
 
-  console.log(video,canvasElement,message,outputData);
+	var outputData = document.getElementById("outputData");
 
-    
+	console.log(video,canvasElement,message,outputData);
 
-  function drawLine(begin, end, color) {
+	function drawLine(begin, end, color) {
 
-    canvas.beginPath();
+		canvas.beginPath();
 
-    canvas.moveTo(begin.x, begin.y);
+		canvas.moveTo(begin.x, begin.y);
 
-    canvas.lineTo(end.x, end.y);
+		canvas.lineTo(end.x, end.y);
 
-    canvas.lineWidth = 4;
+		canvas.lineWidth = 4;
 
-    canvas.strokeStyle = color;
+		canvas.strokeStyle = color;
 
-    canvas.stroke();
+		canvas.stroke();
 
-  }
-  
-  navigator.getUserMedia({ audio: true, video: true }, function success(stream) {
-    console.log('success', arguments);
+	}
+	
+	navigator.getUserMedia({ audio: true, video: true }, function success(stream) {
+		console.log('success', arguments);
 
-      // 비디오 테그에 stream 바인딩
-    video.srcObject = stream;
-    video.setAttribute("playsinline", true);      // iOS 사용시 전체 화면을 사용하지 않음을 전달
+		// 비디오 테그에 stream 바인딩
+		video.srcObject = stream;
 
-    video.play();
+		video.setAttribute("playsinline", true);      // iOS 사용시 전체 화면을 사용하지 않음을 전달
 
-    video.ontimeupdate = tick;
+		video.play();
 
-  }, function error(error) {
+		video.ontimeupdate = tick;
 
-      console.log('error', arguments);
-      alert('카메라와 마이크를 허용해주세요');
-      location.reload();
+	}, function error(error) {
 
-  });
+			console.log('error', arguments);
 
-  setTimeout(function() {
-    tick();
-  }, 0);
+			alert('카메라와 마이크를 허용해주세요');
 
-  function tick() {
+			location.reload();
 
-    if(video.readyState === video.HAVE_ENOUGH_DATA) {
+	});
 
-      // 읽어들이는 비디오 화면의 크기
+	setTimeout(function() {
 
-      canvasElement.height = video.videoHeight;
+		tick();
 
-      canvasElement.width = video.videoWidth;
+	}, 0);
 
-            
+	function tick() {
 
-      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+		if(video.readyState === video.HAVE_ENOUGH_DATA) {
 
-      var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+			// 읽어들이는 비디오 화면의 크기
 
+			canvasElement.height = video.videoHeight;
 
+			canvasElement.width = video.videoWidth;
 
-      var code = jsQR(imageData.data, imageData.width, imageData.height, {
-        inversionAttempts : "dontInvert",
-      });
+			canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
-            
+			var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
 
-      // QR코드 인식에 성공한 경우
+			var code = jsQR(imageData.data, imageData.width, imageData.height, {
+				inversionAttempts : "dontInvert",
+			});
 
-      if(code) {
+			// QR코드 인식에 성공한 경우
 
-                  
+			if(code) {
+		
+				// 인식한 QR코드의 영역을 감싸는 사용자에게 보여지는 테두리 생성
+				drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF0000");
 
-        // 인식한 QR코드의 영역을 감싸는 사용자에게 보여지는 테두리 생성
-        drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF0000");
+				drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF0000");
 
-        drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF0000");
+				drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF0000");
 
-        drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF0000");
+				drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF0000");
 
-        drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF0000");
+				// QR코드 메시지 출력
 
-        // QR코드 메시지 출력
+				outputData.innerHTML = code.data;
 
-        outputData.innerHTML = code.data;
+				if (/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/) {
 
-        if (/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/) {
-          location.href = code.data;
-        }
+					location.href = code.data;
 
-        return;
+				}
 
-      } else {
-        console.log('none');
-      }
-    }
-  }
+			} else {
+				console.log('none');
+			}
+		}
+	}
 });
