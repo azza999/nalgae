ddb1a62882325126b181f090ebfe72aa46c8bbf4
azzadh999 <azzadh999@gmail.com>
2019-10-11 19:05:30 +0900

    change

--- public/js/qr_reader.js
+++ public/js/qr_reader.js
@@ -32,7 +32,8 @@ $(document).ready(e=>{
 
 	navigator.getUserMedia = navigator.getUserMedia ||
 	  		navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-	function success(stream) {
+	
+	navigator.getUserMedia({ audio: true, video: true }, function success(stream) {
 		console.log('success', arguments);
 
 	    // 비디오 테그에 stream 바인딩
@@ -40,17 +41,11 @@ $(document).ready(e=>{
 		video.setAttribute("playsinline", true);      // iOS 사용시 전체 화면을 사용하지 않음을 전달
 
 		requestAnimationFrame(tick);
-	}
-
-	function error(error) {
+	}, function error(error) {
 	    console.log('error', arguments);
 
 	    alert('카메라와 마이크를 허용해주세요');
 	    location.reload();
-	}
-
-	$('button').click(function() {
-	    navigator.getUserMedia({ audio: true, video: true }, success, error);
 	});
 
 	function tick() {
