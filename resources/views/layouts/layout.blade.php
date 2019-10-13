07588928aa4324138ce8e90ee5b6fbb1fb543f61
azzadh999 <azzadh999@gmail.com>
2019-10-14 02:14:26 +0900

    commit

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -28,8 +28,8 @@
 <body>
 	<div class="header-padder"></div>
 	<header>
-		<div class="container d-flex">
-			<div class="logo col-md-12 col-12 d-flex justify-content-between">
+		<div class="container">
+			<div class="logo d-flex justify-content-between">
 				<a class="header-btn" href="{{route('home')}}">
 					<img src="/public/img/home.svg" alt="1">
 				</a>
@@ -40,7 +40,7 @@
 					<img src="/public/img/QRQR.png" alt="qr code reader">
 				</a>
 			</div>
-			<nav class="nav col-12 justify-content-around mt-3">
+			<nav class="nav justify-content-around mt-3">
 				<a href="#" class="nav-item">일정표</a>
 				<a href="#" class="nav-item">퍼레이드</a>
 				<a href="#" class="nav-item">스탬프 투어</a>
