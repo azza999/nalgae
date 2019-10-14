5c59c6e96e31fc6f52e541b843a9eeb7d54153ec
azzadh999 <azzadh999@gmail.com>
2019-10-15 01:36:49 +0900

    modi scroll

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
 	</div>
 	<img src="/public/img/map.jpg" alt="">
 </div>
-<div class="calendar container" id="calendar">
+<div class="calendar container">
 	<div class="section-title">
 		<h2>날개제 일정</h2>
 	</div>
