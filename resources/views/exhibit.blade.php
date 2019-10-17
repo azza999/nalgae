409a7dcdd9df66e412a2d07638dea50ee5c38241
azzadh999 <azzadh999@gmail.com>
2019-10-18 00:58:47 +0900

    end

--- resources/views/exhibit.blade.php
+++ resources/views/exhibit.blade.php
@@ -9,38 +9,46 @@
 		<div class="section-title mb-3">
 			<h2>날개제 전시회</h2>
 		</div>
-		<div class="text-center mt-5 pt-5 mb-5 pb-5">~ 준비중입니다 ~</div>
-		{{-- <div class="container px-5 mb-5">
+		<div class="container px-5 mb-5">
 			<div class="row">
 				<div class="col-12 mb-3">
 					<div class="card">
-						<img src="/public/img/stamp/c1.jpg" alt="" class="card-img-top">
+						<img src="/public/img/stamp/c21.jpg" alt="" class="card-img-top">
 						<div class="card-body">
 							<h5 class="card-title">POP부</h5>
-							<p class="card-text">POP부</p>
+							<p class="card-text"></p>
 						</div>
 					</div>
 				</div>
 				<div class="col-12 mb-3">
 					<div class="card">
-						<img src="/public/img/stamp/c1.jpg" alt="" class="card-img-top">
+						<img src="/public/img/stamp/c22.jpg" alt="" class="card-img-top">
 						<div class="card-body">
 							<h5 class="card-title">미술부</h5>
-							<p class="card-text"></p>
+							<p class="card-text">양인규 선생님과 학생들의 여러 작품을 전시한 전시회 입니다.</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-12 mb-3">
 					<div class="card">
-						<img src="/public/img/stamp/c1.jpg" alt="" class="card-img-top">
+						<img src="/public/img/stamp/c20.jpg" alt="" class="card-img-top">
 						<div class="card-body">
 							<h5 class="card-title">편집부</h5>
-							<p class="card-text"></p>
+							<p class="card-text">학교의 교지인 '날개', '해동청'등 발간자료를 만나볼 수 있는 휴식공간을 운영하고 있습니다.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 mb-3">
+					<div class="card">
+						<img src="/public/img/stamp/c9.jpg" alt="" class="card-img-top">
+						<div class="card-body">
+							<h5 class="card-title">사진부</h5>
+							<p class="card-text">49기부터 51기까지 항과고 학생들의 아름다운 추억을 계절별로 담은 전시작품입니다.</p>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div> --}}
+		</div>
 	</section>
 @endsection
 
